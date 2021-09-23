da8fee051e790dfc0b07fd65c9e81b46e9c9acc6
Bogdan-Kasyanchuk <bogdan.k1990@gmail.com>
2021-09-23 22:05:13 +0300

    Update fnThemeSwitch.js

--- src/js/fnThemeSwitch.js
+++ src/js/fnThemeSwitch.js
@@ -2,7 +2,6 @@ import { bodyEl } from './refs';
 import { LIGHT, DARK } from './theme';
 
 export default function fnThemeSwitch(event) {
-  event.preventDefault();
   if (!event.target.checked) {
     bodyEl.classList.replace(DARK, LIGHT);
   } else if (event.target.checked) {
